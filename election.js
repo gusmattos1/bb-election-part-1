7c8d00372ecd776cc272ca65a9b15d2ce9105f8e
gusmattos1 <gusmattos1@gmail.com>
2018-05-16 16:16:04 -0400

    assignment done

--- election.js
+++ election.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+var button = document.getElementById('button-request')
+result = document.getElementById('result')
 
+var url = 'https://bb-election-api.herokuapp.com/'
+
+button.addEventListener('click', function () {
+
+  $.ajax({
+  			url: url,
+  			method: 'GET',
+  			dataType: 'json'
+  		}).done(function(data){
+        console.log('request done');
+        dados = data
+        result.innerHTML = ''
+        for (var i = 0; i < data.candidates.length; i++) {
+          result.innerHTML += '<p>' + data.candidates[i].name + '<br>' + data.candidates[i].votes + '</p>'
+        }
+      }).fail(function () {
+        console.log('Your request was not successful');
+      });
+});
 });
